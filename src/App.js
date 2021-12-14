e1a7e72fc5668ee90533aea90a0e24b0310bdda0
imamk8114 <imamk8114@gmail.com>
2021-12-14 14:07:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ function App() {
       <Nav/>
       <Banner/>
 
-      <Row title="Netflix Originals"
-           fetchUrl={requests.fetchNetflixOriginals}
-           isLargeRow
-      />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
